0fd794a08ef5c09406a51047c5fab8251cfa8d24
Scute0001 <s12t8811@gmail.com>
2020-12-24 21:13:24 +0800

    modify success dialog add animation

--- app/src/main/java/com/emil/triptrip/ui/addtrip/AddTripFragment.kt
+++ app/src/main/java/com/emil/triptrip/ui/addtrip/AddTripFragment.kt
@@ -6,7 +6,6 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
--- app/src/main/java/com/emil/triptrip/ui/dialog/LoginSuccessDialogFragment.kt
+++ app/src/main/java/com/emil/triptrip/ui/dialog/LoginSuccessDialogFragment.kt
@@ -1,18 +1,15 @@
 package com.emil.triptrip.ui.dialog
 
+import android.animation.Animator
 import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
-import com.emil.triptrip.R
 import com.emil.triptrip.databinding.FragmentLoginSuccessDialogBinding
-import com.emil.triptrip.databinding.LoginFragmentBinding
 
-class LoginSuccessDialogFragment : DialogFragment() {
+class LoginSuccessDialogFragment: DialogFragment() {
 
     override fun onStart() {
         super.onStart()
@@ -26,12 +23,34 @@ class LoginSuccessDialogFragment : DialogFragment() {
         // Inflate the layout for this fragment
         val binding = FragmentLoginSuccessDialogBinding.inflate(inflater, container, false)
 
+        // lottile setting
+        binding.dialogLottie.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
 
-        Handler().postDelayed(
-            {dismiss()
+            override fun onAnimationEnd(animation: Animator?) {
+                dismiss()
                 findNavController().navigate(LoginSuccessDialogFragmentDirections.actionLoginSuccessDialogFragmentToMyTripsFragment())
-            },
-            1000)
+            }
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+        })
+
+        binding.dialogLottie.apply {
+            speed = 1.7F
+            playAnimation()
+        }
+
+
+//        Handler().postDelayed(
+//            {dismiss()
+//                findNavController().navigate(LoginSuccessDialogFragmentDirections.actionLoginSuccessDialogFragmentToMyTripsFragment())
+//            },
+//            1000)
 
         return binding.root
     }
