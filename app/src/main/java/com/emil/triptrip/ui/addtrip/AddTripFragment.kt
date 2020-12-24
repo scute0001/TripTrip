7d8e1a2c6fb50e5495c0fb8a9f4449d84bb3060d
Scute0001 <s12t8811@gmail.com>
2020-12-24 20:20:41 +0800

    add add trip success animation

--- app/src/main/java/com/emil/triptrip/ui/addtrip/AddTripFragment.kt
+++ app/src/main/java/com/emil/triptrip/ui/addtrip/AddTripFragment.kt
@@ -1,10 +1,12 @@
 package com.emil.triptrip.ui.addtrip
 
 import android.Manifest
+import android.animation.Animator
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -83,12 +85,34 @@ class AddTripFragment : Fragment() {
             }
         })
 
+        // lottile setting
+        binding.lottieSuccess.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                findNavController().navigate(AddTripFragmentDirections.actionAddTripFragmentToMyTripsFragment())
+                viewModel.clearAddTripFinishedNavToMyTrips()
+                binding.constraintSuccess.visibility = View.GONE
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+
+        })
+
         // add data finished and nav to mytrips
         viewModel.addTripFinishedNavToMyTrips.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             if (it == true) {
                 Toast.makeText(requireContext(), "新增旅程成功", Toast.LENGTH_SHORT).show()
-                findNavController().navigate(AddTripFragmentDirections.actionAddTripFragmentToMyTripsFragment())
-                viewModel.clearAddTripFinishedNavToMyTrips()
+                binding.constraintSuccess.visibility = View.VISIBLE
+                binding.lottieSuccess.playAnimation()
             }
         })
 
